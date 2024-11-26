13f072fb17b913ac99b66aed5b1d8113a14d8d7a
GKefas <kefasgeo04@gmail.com>
2024-11-26 16:07:41 +0200

    Initial commit

---
+++ src/main/java/com/gkefas/trackmanager/TrackManagerApplication.java
@@ -0,0 +1,13 @@
+package com.gkefas.trackmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gkefas/trackmanager/TrackManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gkefas.trackmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
