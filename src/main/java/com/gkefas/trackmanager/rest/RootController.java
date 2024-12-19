271cd17ed7b2f667d4b95a8a88f929a251619a2c
GKefas <kefasgeo04@gmail.com>
2024-12-20 00:10:20 +0200

    Adding Documentation Page

--- src/main/java/com/gkefas/trackmanager/rest/RootController.java
+++ src/main/java/com/gkefas/trackmanager/rest/RootController.java
@@ -1,12 +1,12 @@
 package com.gkefas.trackmanager.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
 	@GetMapping({"/api", "/api/"})
 	public String api() {
-		return "api";
+		return "index";
 	}
 }
